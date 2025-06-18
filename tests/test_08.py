db3efb0b4293ea0a24e2c8018e08d0d956974351
arthurrochamoreira <altinoarthurmoreira@gmail.com>
2025-06-18 13:15:26 -0300

    AJUSTE DNV

--- tests/test_08.py
+++ tests/test_08.py
@@ -1,84 +1,42 @@
-import builtins
-
 from lox import *
 from lox import testing
 from lox.ast import *
 
 
-class TestSetattr(testing.ExerciseTester):
-    is_expr = True
-    src1 = "obj.attr = 42"
-    src2 = "func(obj).attr = 42"
-    src3 = "obj.attr.subattr = 42"
-    tks1 = "obj attr 42"
-    tks2 = "func obj attr 42"
-    tks3 = "obj attr subattr 42"
-    ast_class = Setattr
+class TestVarDef(testing.ExerciseTester):
+    is_expr = False
+    src1 = "var x = 42;"
+    src2 = "var y;"
+    src3 = "var x = 40 + 2;"
+    tks1 = "x 42"
+    tks2 = "y"
+    tks3 = "x 40 2"
+    ast_class = VarDef
 
     def eval_env(self, n):
-        ctx = {"func": lambda x: x, "obj": Obj()}
-        src = self.src(n)
-        builtins.exec(src, ctx.copy())
-        return (ctx, 42.0)
+        result = [{"x": 42}, {"y": None}, {"x": 42}][n - 1]
+        return ({}, result)
 
-    def test_atribuição_de_atributo_modifica_objeto(self):
-        obj = Obj()
-        ctx = {"obj": obj}
-        print(ctx)
-        expect = Obj(attr=Obj(subattr=42.0))
-        self.verify("obj.attr.subattr = 42", ctx, expect_none=True)
-        ast = self.parse("obj.attr.subattr = 42")
-        ast.eval(Ctx.from_dict({"obj": obj}))
-        assert obj == expect
+    def test_declaração_de_variáveis_modifica_o_contexto(self):
+        ctx = Ctx.from_dict({})
+        ast = self.ast(1)
+        ast.eval(ctx)
+        assert ctx["x"] == 42, "Atribuição não modificou o contexto corretamente"
 
-
-class Obj:
-    def __init__(self, **kwargs):
-        self._data = kwargs
-
-    def __getattr__(self, attr):
-        # prevent some pytest magic with mocks
-        if attr == "awehoi234_wdfjwljet234_234wdfoijsdfmmnxpi492":
-            raise AttributeError
-        if attr.startswith("_"):
-            raise AttributeError
-
-        try:
-            return self._data[attr]
-        except KeyError:
-            value = Obj()
-            self._data[attr] = value
-            return value
-
-    def __setattr__(self, attr, value):
-        if attr.startswith("_"):
-            super().__setattr__(attr, value)
-        else:
-            self._data[attr] = value
-
-    def __eq__(self, other):
-        if isinstance(other, Obj):
-            return self._data == other._data
-        return NotImplemented
-
-    def __repr__(self):
-        data = ", ".join(f"{k}={v!r}" for k, v in self._data.items())
-        return f"Obj({data})"
-
-    def __str__(self):
-        return repr(self)
+    def test_declaração_de_variáveis_não_inicializadas_inicializa_o_valor_com_nil(self):
+        ctx = Ctx.from_dict({})
+        ast = self.ast(2)
+        ast.eval(ctx)
+        assert ctx["y"] is None
 
 
 class TestExamples(testing.ExampleTester):
-    module = "field"
-    examples = [
-        "get_on_bool",
-        "get_on_nil",
-        "get_on_num",
-        "get_on_string",
-        "set_on_bool",
-        "set_on_nil",
-        "set_on_num",
-        "set_on_string",
-    ]
+    module = "variable"
+    examples = {
+        # "redeclare_global",
+        "redefine_global",
+        "undefined_global",
+        "uninitialized",
+        "use_global_in_initializer",
+    }
     fuzzy_output = True
